1f855f3c177d2f5d5635b123c4616e22739f7da4
zzyhar <93952085+zzyhar@users.noreply.github.com>
2023-01-08 13:38:47 -0500

    Tables and Headers for Contact and Team Pages

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-// import Team from "./scenes/team";
-// import Contacts from "./scenes/contacts";
+import Team from "./scenes/team";
+import Contacts from "./scenes/contacts";
 // import Invoices from "./scenes/invoices";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
@@ -27,8 +27,8 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} /> */}
-              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
               {/* <Route path="/invoices" element={<Invoices />} /> */}
               {/* <Route path="/form" element={<Form />} /> */}
               {/* <Route path="/bar" element={<Bar />} /> */}
