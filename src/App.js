07a40474876851590d681080d37de5e6231e07bb
zzyhar <93952085+zzyhar@users.noreply.github.com>
2023-01-10 00:12:35 -0500

    Line Chart and Geography Map using Nivo Charts

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ import Form from "./scenes/form";
 import Calendar from "./scenes/calendar";
 import FAQ from "./scenes/faq";
 import Bar from "./scenes/bar";
-// import Line from "./scenes/line";
 import Pie from "./scenes/pie";
-// import Geography from "./scenes/geography";
+import Line from "./scenes/line";
+import Geography from "./scenes/geography";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -35,8 +35,8 @@ function App() {
               <Route path="/faq" element={<FAQ />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
-              {/* <Route path="/line" element={<Line />} /> */}
-              {/* <Route path="/geography" element={<Geography />} /> */}
+              <Route path="/line" element={<Line />} />
+              <Route path="/geography" element={<Geography />} />
             </Routes>
           </main>
         </div>
